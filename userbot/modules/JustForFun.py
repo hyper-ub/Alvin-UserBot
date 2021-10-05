17865791032aeb9da81868ed831647c994123b9b
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-10-05 17:00:08 +0800

    Update JustForFun.py

--- userbot/modules/JustForFun.py
+++ userbot/modules/JustForFun.py
@@ -139,7 +139,7 @@ async def _(event):
 @bot.on(events.NewMessage(pattern=r"\;(;*)", outgoing=True))
 async def _(event):
 
-    if event.fwd_from:;
+    if event.fwd_from:
         return
 
     animation_interval = 0.3
