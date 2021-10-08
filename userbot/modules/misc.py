cfdb3e89b4d3df928096f1a8d6b2ce74ebe34e5a
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-10-08 10:42:10 +0800

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -59,7 +59,7 @@ async def sleepybot(time):
 @register(outgoing=True, pattern="^;shutdown$")
 async def killdabot(event):
     """ For .shutdown command, shut the bot down."""
-    await event.edit("`Mematikan Alvin-Userbot....`")
+    await event.edit("`shuting down Alvin-Userbot....`")
     await asyncio.sleep(7)
     await event.delete()
     if BOTLOG:
