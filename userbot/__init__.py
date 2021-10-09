bdb428e0f530947d85842e4b3888590f243248ce
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-10-09 15:28:49 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -166,7 +166,7 @@ BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 TERM_ALIAS = os.environ.get("TERM_ALIAS", "Alvin-UserBot")
 
 # Bot version
-BOT_VER = os.environ.get("BOT_VER", "5.3")
+BOT_VER = os.environ.get("BOT_VER", "7.0")
 
 # Default .alive username
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
